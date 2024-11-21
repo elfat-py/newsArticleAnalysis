b229e144923a266eaec0ce94af981b582c76ac22
Elfat Memaj <elfatmemaj@gmail.com>
2024-11-21 20:15:14 +0100

    Making loop on id wtf

--- scrapeNews/scrapeNews/spiders/topChannel.py
+++ scrapeNews/scrapeNews/spiders/topChannel.py
@@ -2,9 +2,16 @@ import scrapy
 
 
 class TopchannelSpider(scrapy.Spider):
-    name = "topChannel"
+    name = "topja"
     allowed_domains = ["top-channel.tv"]
     start_urls = ["https://top-channel.tv/"]
 
     def parse(self, response):
-        pass
+        categories = [response.css('#newsMenu1'), response.css('#newsMenu2')]  # Combine categories
+        #response.css("#newsMenu1 > .menu-item > a::attr(href)").getall()
+        for category in categories:
+            name = category.css('::text').get().strip()
+            link = category.css('::attr(href)').get()
+            full_link = response.urljoin(link)
+
+            yield name, full_link, link
